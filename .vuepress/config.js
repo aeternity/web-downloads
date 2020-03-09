2e610b7009556c585e460ec67ce2cee412dd5e93
Ivaylo Papazov <papazov@vmandco.com>
2020-03-09 08:46:58 +0200

    Add base path

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,9 +1,12 @@
+let basePath = process.env.BASE_PATH || '/';
+
 module.exports = {
     title: 'Aeternity Downloads',
     description: 'Aeternity',
     themeConfig: {
         search: false,
     },
+    base: basePath,
     plugins: [
         '@vuepress/register-components',
     ],
